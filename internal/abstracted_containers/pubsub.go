c1cecaf6d96484b9f4915d10fcfc90b0c6aeace2
Josep Bigorra <jjbigorra@gmail.com>
2022-10-27 14:34:53 +0200

    further work into pubsub

--- internal/abstracted_containers/pubsub.go
+++ internal/abstracted_containers/pubsub.go
@@ -3,7 +3,7 @@ package abstractedcontainers
 import (
 	"context"
 	"fmt"
-	"log"
+	"sync/atomic"
 	"time"
 
 	"cloud.google.com/go/pubsub"
@@ -18,10 +18,19 @@ type PubSubContainer struct {
 
 func SetupPubsub(ctx context.Context) (*PubSubContainer, error) {
 	req := testcontainers.ContainerRequest{
-		Image:        "bigtruedata/gcloud-pubsub-emulator",
-		ExposedPorts: []string{"8538/tcp"},
-		WaitingFor:   wait.ForLog("[pubsub] INFO: Server started, listening on 8538"),
-		NetworkMode:  "bridge",
+		Image:        "thekevjames/gcloud-pubsub-emulator:latest",
+		ExposedPorts: []string{"8682/tcp", "8681/tcp"},
+		Env: map[string]string{
+			// "PUBSUB_PROJECT1": "PROJECTID,TOPIC1,TOPIC2:SUBSCRIPTION1:SUBSCRIPTION2,TOPIC3:SUBSCRIPTION3"
+			// "PUBSUB_PROJECT1": "myProject,myTopic:mySub",
+			"PUBSUB_PROJECT1": "myProject,myTopic",
+		},
+		//Hostname:     "0.0.0.0",
+		//WaitingFor:   wait.ForLog("[pubsub] INFO: Server started, listening on 8538"),
+		//NetworkMode:  "bridge",
+		WaitingFor: wait.NewHostPortStrategy("8682"),
+		//Cmd:        []string{"gcloud beta emulators pubsub start --project=test_project --host-port=0.0.0.0:8085"},
+		// Cmd: []string{"gcloud beta emulators pubsub start --project=test_project --host-port=0.0.0.0:8085"},
 	}
 
 	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
@@ -37,46 +46,47 @@ func SetupPubsub(ctx context.Context) (*PubSubContainer, error) {
 		return nil, err
 	}
 
-	mappedPort, err := container.MappedPort(ctx, "8538")
+	mappedPort, err := container.MappedPort(ctx, "8681")
 	if err != nil {
 		return nil, err
 	}
 
 	uri := fmt.Sprintf("%s:%s", ip, mappedPort.Port())
 	// uri := fmt.Sprintf("172.17.0.1:%s", mappedPort.Port())
-	log.Printf("New container started, accessible at: %s", uri)
+	fmt.Printf("New container started, accessible at: %s", uri)
 
 	return &PubSubContainer{Container: container, URI: uri}, nil
 }
 
 func GetOrCreateTopic(ctx context.Context, uri, topicID string) (*pubsub.Topic, error) {
-	client, err := pubsub.NewClient(ctx, "test_project")
+	client, err := pubsub.NewClient(context.TODO(), "myProject")
 	if err != nil {
 		return nil, err
 	}
 
-	log.Println("Pub/Sub client created")
-	log.Printf("%+v", client)
+	fmt.Println("Pub/Sub client created")
+	fmt.Printf("%+v", client)
 
 	defer client.Close()
 
 	topic := client.Topic(topicID)
 
 	// Create the topic if it doesn't exist.
-	// exists, err := topic.Exists(ctx)
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	// if !exists {
-	log.Printf("Topic %v doesn't exist - creating it", topicID)
-
-	if _, err = client.CreateTopic(ctx, topicID); err != nil {
+	exists, err := topic.Exists(context.TODO())
+	if err != nil {
 		return nil, err
 	}
-	// }
 
-	log.Printf("%+v", topic)
+	if !exists {
+		fmt.Printf("Topic %v doesn't exist - creating it", topicID)
+
+		if _, err = client.CreateTopic(context.TODO(), topicID); err != nil {
+			return nil, err
+		}
+	} else {
+		fmt.Println("Not required to create topic, it exists")
+	}
+
 	return topic, nil
 }
 
@@ -85,49 +95,80 @@ func SendMessageToPubSub(ctx context.Context, uri string, topic *pubsub.Topic) e
 		Data: []byte("This is a test message!"),
 	}
 
-	log.Printf("%+v", topic)
-	log.Println("About to publish message!")
+	fmt.Println("About to publish message!")
 
-	if _, err := topic.Publish(ctx, msg).Get(ctx); err != nil {
-		return err
-	}
+	// if _, err := topic.Publish(context.TODO(), msg).Get(context.TODO()); err != nil {
+	// 	return err
+	// }
 
-	log.Println("Message published.")
+	res := topic.Publish(context.TODO(), msg)
+	// if _, err := .Get(context.TODO()); err != nil {
+	// 	return err
+	// }
+
+	fmt.Printf("%+v", res)
+
+	fmt.Println("Message published.")
 	return nil
 }
 
 func CheckMessageReceived(ctx context.Context, uri string, topic *pubsub.Topic, subscriptionID string) error {
-	client, err := pubsub.NewClient(ctx, "test_project")
+	client, err := pubsub.NewClient(context.TODO(), "myProject")
 	if err != nil {
 		return err
 	}
+
+	defer client.Close()
 
 	sub, err := client.CreateSubscription(ctx, subscriptionID, pubsub.SubscriptionConfig{
 		Topic: topic,
 	})
 
-	cctx, cancel := context.WithCancel(ctx)
-	okCh := make(chan string)
+	// sub := client.Subscription("mySub")
 
-	go func() {
-		// Use a callback to receive messages via subscription.
-		// Receive will block until the context is cancelled, or we get a non-recoverable error
-		err = sub.Receive(cctx, func(_ context.Context, m *pubsub.Message) {
-			m.Ack()
-			okCh <- string(m.Data)
-			cancel()
-		})
-	}()
+	// Receive messages for 10 seconds, which simplifies testing.
+	// Comment this out in production, since `Receive` should
+	// be used as a long running operation.
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
 
-	select {
-	case msg := <-okCh:
-		if msg != "This is a test message!" {
-			return fmt.Errorf("")
-		}
-	case <-time.After(300 * time.Millisecond):
-		cancel()
-		return fmt.Errorf("did not receive message within deadline")
+	var received int32
+
+	err = sub.Receive(ctx, func(_ context.Context, msg *pubsub.Message) {
+		fmt.Printf("Got message: %q\n", string(msg.Data))
+		atomic.AddInt32(&received, 1)
+		msg.Ack()
+	})
+	if err != nil {
+		return fmt.Errorf("sub.Receive: %v", err)
 	}
 
+	fmt.Printf("Received %d messages\n", received)
+
 	return nil
+
+	// cctx, cancel := context.WithCancel(context.TODO())
+	// okCh := make(chan string)
+
+	// go func() {
+	// 	// Use a callback to receive messages via subscription.
+	// 	// Receive will block until the context is cancelled, or we get a non-recoverable error
+	// 	err = sub.Receive(cctx, func(_ context.Context, m *pubsub.Message) {
+	// 		m.Ack()
+	// 		okCh <- string(m.Data)
+	// 		cancel()
+	// 	})
+	// }()
+
+	// select {
+	// case msg := <-okCh:
+	// 	if msg != "This is a test message!" {
+	// 		return fmt.Errorf("")
+	// 	}
+	// case <-time.After(300000 * time.Millisecond):
+	// 	cancel()
+	// 	return fmt.Errorf("did not receive message within deadline")
+	// }
+
+	// return nil
 }
--- internal/abstracted_containers/pubsub_test.go
+++ internal/abstracted_containers/pubsub_test.go
@@ -3,22 +3,40 @@ package abstractedcontainers_test
 // Basic imports
 import (
 	"context"
-	"log"
+	"fmt"
 	"os"
 	"testing"
 	"time"
 
 	abstractedcontainers "github.com/averageflow/sakerhet/internal/abstracted_containers"
-	"github.com/google/uuid"
 )
 
+// func TestWithRedis(t *testing.T) {
+// 	ctx := context.Background()
+// 	req := testcontainers.ContainerRequest{
+// 		Image:        "redis:latest",
+// 		ExposedPorts: []string{"6379/tcp"},
+// 		WaitingFor:   wait.ForLog("Ready to accept connections"),
+// 	}
+// 	redisC, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+// 		ContainerRequest: req,
+// 		Started:          true,
+// 	})
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+// 	defer redisC.Terminate(ctx)
+// }
+
 func TestExample(t *testing.T) {
-	topicID := "topic-" + uuid.New().String()
-	subscriptionID := "sub-" + uuid.New().String()
+	// topicID := "topic-" + uuid.New().String()
+	// subscriptionID := "sub-" + uuid.New().String()
+	topicID := "myTopic"
+	subscriptionID := "mySub"
 
-	log.Printf("topic: %s , subscription: %s", topicID, subscriptionID)
+	fmt.Printf("topic: %s , subscription: %s", topicID, subscriptionID)
 
-	ctx, _ := context.WithTimeout(context.Background(), 20*time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), 2000*time.Second)
 
 	pubSubC, err := abstractedcontainers.SetupPubsub(ctx)
 	if err != nil {
@@ -26,22 +44,26 @@ func TestExample(t *testing.T) {
 	}
 
 	os.Setenv("PUBSUB_EMULATOR_HOST", pubSubC.URI)
+	// os.Setenv("PUBSUB_EMULATOR_HOST", "localhost:8085")
 
 	// Clean up the container after the test is complete
 	defer pubSubC.Terminate(ctx)
 
 	topic, err := abstractedcontainers.GetOrCreateTopic(ctx, pubSubC.URI, topicID)
+	// topic, err := abstractedcontainers.GetOrCreateTopic(ctx, "localhost:8085", topicID)
 	if err != nil {
 		t.Error(err)
 		return
 	}
 
 	if err := abstractedcontainers.SendMessageToPubSub(ctx, pubSubC.URI, topic); err != nil {
+		// if err := abstractedcontainers.SendMessageToPubSub(ctx, "localhost:8085", topic); err != nil {
 		t.Error(err)
 		return
 	}
 
 	if err := abstractedcontainers.CheckMessageReceived(ctx, pubSubC.URI, topic, subscriptionID); err != nil {
+		// if err := abstractedcontainers.CheckMessageReceived(ctx, "localhost:8085", topic, subscriptionID); err != nil {
 		t.Error(err)
 		return
 	}
