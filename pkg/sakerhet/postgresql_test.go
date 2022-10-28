26ed09a26e5b82420fdebb26ec283a7b60c94d65
Josep Bigorra <jjbigorra@gmail.com>
2022-10-28 11:23:32 +0200

    add postgresql low level test

--- pkg/sakerhet/postgresql_test.go
+++ pkg/sakerhet/postgresql_test.go
@@ -3,6 +3,7 @@ package sakerhet_test
 import (
 	"context"
 	"fmt"
+	"reflect"
 	"testing"
 	"time"
 
@@ -13,6 +14,7 @@ import (
 func TestLowLevelIntegrationTestPostgreSQL(t *testing.T) {
 	t.Parallel()
 
+	// given
 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(time.Second*15))
 	defer cancel()
 
@@ -40,10 +42,9 @@ func TestLowLevelIntegrationTestPostgreSQL(t *testing.T) {
 		CREATE TABLE accounts (
 				user_id serial PRIMARY KEY,
 				username VARCHAR ( 50 ) UNIQUE NOT NULL,
-				password VARCHAR ( 50 ) NOT NULL,
 				email VARCHAR ( 255 ) UNIQUE NOT NULL,
 				age INTEGER NOT NULL,
-	      created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+	      created_on INTEGER NOT NULL DEFAULT extract(epoch from now())
     );
 		`,
 	}
@@ -52,11 +53,54 @@ func TestLowLevelIntegrationTestPostgreSQL(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	insertQuery := `INSERT INTO accounts (username, password, email, age) VALUES ($1, $2, $3, $4);`
+	insertQuery := `INSERT INTO accounts (username, email, age, created_on) VALUES ($1, $2, $3, $4);`
 	seedData := [][]any{
-		{"myUser", "myPassword", "myEmail", 25},
+		{"myUser", "myEmail", 25, 1234567},
 	}
 	if err := abstractedcontainers.InitPostgreSQLDataInTable(ctx, dbpool, insertQuery, seedData); err != nil {
 		t.Fatal(err)
+	}
+
+	type account struct {
+		userId    int
+		username  string
+		email     string
+		age       int
+		createdOn int
+	}
+
+	// when
+	getQuery := `SELECT user_id, username, email, age, created_on FROM accounts;`
+
+	rows, err := dbpool.Query(ctx, getQuery)
+	defer rows.Close()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var result []account
+
+	for rows.Next() {
+		var acc account
+
+		if err := rows.Scan(&acc.userId, &acc.username, &acc.email, &acc.age, &acc.createdOn); err != nil {
+			t.Fatal(err)
+		}
+
+		result = append(result, acc)
+	}
+
+	// then
+	expected := []account{
+		{userId: 1, username: "myUser", email: "myEmail", age: 25, createdOn: 1234567},
+	}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Fatal(fmt.Errorf(
+			"received data is different than expected:\n received %v\n expected %v\n",
+			result,
+			expected,
+		))
 	}
 }
