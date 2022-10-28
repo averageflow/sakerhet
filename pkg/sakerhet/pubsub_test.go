b7ff60ceb2d470a7d4799fd44152e5d01c3dc51a
Josep Bigorra <jjbigorra@gmail.com>
2022-10-28 13:20:22 +0200

    smart constructors

--- pkg/sakerhet/pubsub_test.go
+++ pkg/sakerhet/pubsub_test.go
@@ -54,67 +54,13 @@ func TestHighLevelIntegrationTestGCPPubSub(t *testing.T) {
 	}
 }
 
-// Low level test with full control on testing code that pushes to Pub/Sub
-func TestLowLevelIntegrationTestGCPPubSub(t *testing.T) {
-	// t.Parallel() not possible yet due to os.Setenv("PUBSUB_EMULATOR_HOST", pubSubC.URI)
-
-	projectID := "test-project"
-	topicID := "test-topic-" + uuid.New().String()
-	subscriptionID := "test-sub-" + uuid.New().String()
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(time.Second*15))
-	defer cancel()
-
-	topicSubscriptionMap := map[string][]string{
-		topicID: {subscriptionID},
-	}
-
-	pubSubC, err := abstractedcontainers.SetupGCPPubsub(ctx, projectID, topicSubscriptionMap)
-	if err != nil {
-		t.Error(err)
-	}
-
-	// clean up the container after the test is complete
-	defer func() {
-		_ = pubSubC.Terminate(ctx)
-	}()
-
-	fmt.Printf("New container started, accessible at: %s\n", pubSubC.URI)
-
-	// required so that all Pub/Sub calls go to docker container, and not GCP
-	os.Setenv("PUBSUB_EMULATOR_HOST", pubSubC.URI)
-
-	client, err := pubsub.NewClient(context.TODO(), projectID)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	defer client.Close()
-
-	topic, err := abstractedcontainers.GetOrCreateGCPTopic(ctx, client, topicID)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	wantedData := []byte(`{"myKey": "myValue"}`)
-
-	if err := abstractedcontainers.PublishToGCPTopic(ctx, client, topic, wantedData); err != nil {
-		t.Fatal(err)
-	}
-
-	expectedData := [][]byte{[]byte(wantedData)}
-	if err := abstractedcontainers.CheckGCPMessageInSub(ctx, client, subscriptionID, expectedData, 1000*time.Millisecond); err != nil {
-		t.Fatal(err)
-	}
-}
-
 // simple service that does a computation and publishes to Pub/Sub
 type myPowerOfNService struct {
 	toPowerOfN    func(float64, float64) float64
 	publishResult func(context.Context, string, string, float64) error
 }
 
-func NewMyPowerOfNService() myPowerOfNService {
+func newMyPowerOfNService() myPowerOfNService {
 	return myPowerOfNService{
 		toPowerOfN: func(x float64, n float64) float64 { return math.Pow(x, n) },
 		publishResult: func(ctx context.Context, projectID, topicID string, x float64) error {
@@ -164,7 +110,7 @@ func TestHighLevelIntegrationTestOfServiceThatUsesGCPPubSub(t *testing.T) {
 	}()
 
 	// when
-	ponService := NewMyPowerOfNService()
+	ponService := newMyPowerOfNService()
 
 	if err := ponService.publishResult(
 		ctx,
@@ -181,6 +127,63 @@ func TestHighLevelIntegrationTestOfServiceThatUsesGCPPubSub(t *testing.T) {
 		1000*time.Millisecond,
 		expectedData,
 	); err != nil {
+		t.Fatal(err)
+	}
+}
+
+// Low level test with full control on testing code that pushes to Pub/Sub
+func TestLowLevelIntegrationTestGCPPubSub(t *testing.T) {
+	// t.Parallel() not possible yet due to os.Setenv("PUBSUB_EMULATOR_HOST", pubSubC.URI)
+
+	// given
+	projectID := "test-project"
+	topicID := "test-topic-" + uuid.New().String()
+	subscriptionID := "test-sub-" + uuid.New().String()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(time.Second*15))
+	defer cancel()
+
+	topicSubscriptionMap := map[string][]string{
+		topicID: {subscriptionID},
+	}
+
+	pubSubC, err := abstractedcontainers.SetupGCPPubsub(ctx, projectID, topicSubscriptionMap)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// clean up the container after the test is complete
+	defer func() {
+		_ = pubSubC.Terminate(ctx)
+	}()
+
+	fmt.Printf("New container started, accessible at: %s\n", pubSubC.URI)
+
+	// required so that all Pub/Sub calls go to docker container, and not GCP
+	os.Setenv("PUBSUB_EMULATOR_HOST", pubSubC.URI)
+
+	client, err := pubsub.NewClient(context.TODO(), projectID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer client.Close()
+
+	topic, err := abstractedcontainers.GetOrCreateGCPTopic(ctx, client, topicID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// when
+	wantedData := []byte(`{"myKey": "myValue"}`)
+
+	if err := abstractedcontainers.PublishToGCPTopic(ctx, client, topic, wantedData); err != nil {
+		t.Fatal(err)
+	}
+
+	// theNn
+	expectedData := [][]byte{[]byte(wantedData)}
+	if err := abstractedcontainers.CheckGCPMessageInSub(ctx, client, subscriptionID, expectedData, 1000*time.Millisecond); err != nil {
 		t.Fatal(err)
 	}
 }
