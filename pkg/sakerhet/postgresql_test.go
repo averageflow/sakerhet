e3f015ef1a7e768af04dc80cb032b8e914ec33b0
Josep Bigorra <jjbigorra@gmail.com>
2022-10-31 11:21:40 +0100

    suite

--- pkg/sakerhet/postgresql_test.go
+++ pkg/sakerhet/postgresql_test.go
@@ -10,30 +10,118 @@ import (
 	abstractedcontainers "github.com/averageflow/sakerhet/pkg/abstracted_containers"
 	"github.com/averageflow/sakerhet/pkg/sakerhet"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestHighLevelIntegrationTestPostgreSQL(t *testing.T) {
-	// t.Parallel()
+type PostgreSQLTestSuite struct {
+	suite.Suite
+	TestContext         context.Context
+	PostgreSQLContainer *abstractedcontainers.PostgreSQLContainer
+	IntegrationTester   sakerhet.IntegrationTester
+}
 
-	// given
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(time.Second*15))
-	defer cancel()
+// before each test
+func (suite *PostgreSQLTestSuite) SetupSuite() {
+	ctx, _ := context.WithTimeout(context.Background(), time.Duration(time.Second*30))
+	suite.TestContext = ctx
 
-	i := sakerhet.NewIntegrationTest(sakerhet.IntegrationTestParams{
+	suite.IntegrationTester = sakerhet.NewIntegrationTest(sakerhet.IntegrationTesterParams{
 		TestContext: ctx,
 		PostgreSQL:  &sakerhet.PostgreSQLIntegrationTestParams{},
 	})
-	i.PostgreSQLIntegrationTester.PrintSomet()
 
-	postgreSQLC, err := abstractedcontainers.SetupPostgreSQL(ctx)
+	postgreSQLC, err := suite.IntegrationTester.PostgreSQLIntegrationTester.ContainerStart()
 	if err != nil {
-		t.Fatal(err)
+		suite.T().Fatal(err)
 	}
 
-	// clean up the container after the test is complete
-	defer func() {
-		_ = postgreSQLC.Terminate(ctx)
-	}()
+	suite.PostgreSQLContainer = postgreSQLC
+}
+
+func (suite *PostgreSQLTestSuite) TearDownSuite() {
+	suite.PostgreSQLContainer.Terminate(suite.TestContext)
+}
+
+func TestPostgreSQLTestSuite(t *testing.T) {
+	suite.Run(t, new(PostgreSQLTestSuite))
+}
+
+func (suite *PostgreSQLTestSuite) TestHighLevelIntegrationTestPostgreSQL() {
+	dbpool, err := pgxpool.New(suite.TestContext, suite.PostgreSQLContainer.PostgreSQLConnectionURL)
+	if err != nil {
+		suite.T().Fatal(fmt.Errorf("Unable to create connection pool: %v\n", err))
+	}
+
+	defer dbpool.Close()
+
+	initialSchema := []string{
+		`
+		CREATE TABLE accounts (
+				user_id serial PRIMARY KEY,
+				username VARCHAR ( 50 ) UNIQUE NOT NULL,
+				email VARCHAR ( 255 ) UNIQUE NOT NULL,
+				age INTEGER NOT NULL,
+	      created_on INTEGER NOT NULL DEFAULT extract(epoch from now())
+    );
+		`,
+	}
+
+	if err := abstractedcontainers.InitPostgreSQLSchema(suite.TestContext, dbpool, initialSchema); err != nil {
+		suite.T().Fatal(err)
+	}
+
+	insertQuery := `INSERT INTO accounts (username, email, age, created_on) VALUES ($1, $2, $3, $4);`
+	seedData := [][]any{
+		{"myUser", "myEmail", 25, 1234567},
+	}
+
+	// when
+	if err := abstractedcontainers.InitPostgreSQLDataInTable(suite.TestContext, dbpool, insertQuery, seedData); err != nil {
+		suite.T().Fatal(err)
+	}
+
+	// then
+	type account struct {
+		userId    int
+		username  string
+		email     string
+		age       int
+		createdOn int
+	}
+
+	getQuery := `SELECT user_id, username, email, age, created_on FROM accounts;`
+
+	rows, err := dbpool.Query(suite.TestContext, getQuery)
+	if err != nil {
+		suite.T().Fatal(err)
+	}
+
+	defer rows.Close()
+
+	var result []account
+
+	for rows.Next() {
+		var acc account
+
+		if err := rows.Scan(&acc.userId, &acc.username, &acc.email, &acc.age, &acc.createdOn); err != nil {
+			suite.T().Fatal(err)
+		}
+
+		result = append(result, acc)
+	}
+
+	// then
+	expected := []account{
+		{userId: 1, username: "myUser", email: "myEmail", age: 25, createdOn: 1234567},
+	}
+
+	if !reflect.DeepEqual(result, expected) {
+		suite.T().Fatal(fmt.Errorf(
+			"received data is different than expected:\n received %v\n expected %v\n",
+			result,
+			expected,
+		))
+	}
 }
 
 func TestLowLevelIntegrationTestPostgreSQL(t *testing.T) {
--- pkg/sakerhet/pubsub_test.go
+++ pkg/sakerhet/pubsub_test.go
@@ -16,160 +16,90 @@ import (
 	"google.golang.org/api/option"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	"gotest.tools/assert"
 )
 
-// Define the suite, and absorb the built-in basic suite
-// functionality from testify - including a T() method which
-// returns the current testing context
-type ExampleTestSuite struct {
+type GCPPubSubTestSuite struct {
 	suite.Suite
-	VariableThatShouldStartAtFive int
+	TestContext        context.Context
+	GCPPubSubContainer *abstractedcontainers.GCPPubSubContainer
+	IntegrationTester  sakerhet.IntegrationTester
 }
 
-// Make sure that VariableThatShouldStartAtFive is set to five
 // before each test
-func (suite *ExampleTestSuite) SetupTest() {
-	suite.VariableThatShouldStartAtFive = 5
-}
+func (suite *GCPPubSubTestSuite) SetupSuite() {
+	ctx, _ := context.WithTimeout(context.Background(), time.Duration(time.Second*30))
+	suite.TestContext = ctx
 
-// All methods that begin with "Test" are run as tests within a
-// suite.
-func (suite *ExampleTestSuite) TestExample() {
-	assert.Equal(suite.T(), 5, suite.VariableThatShouldStartAtFive)
-}
-
-// In order for 'go test' to run this suite, we need to create
-// a normal test function and pass our suite to suite.Run
-func TestExampleTestSuite(t *testing.T) {
-	suite.Run(t, new(ExampleTestSuite))
-}
-
-// High level test on code that pushes to Pub/Sub
-func TestHighLevelIntegrationTestGCPPubSub(t *testing.T) {
-	// t.Parallel()
-
-	// given
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(time.Second*30))
-	defer cancel()
-
-	i := sakerhet.NewIntegrationTest(sakerhet.IntegrationTestParams{
+	suite.IntegrationTester = sakerhet.NewIntegrationTest(sakerhet.IntegrationTesterParams{
 		TestContext: ctx,
 		GCPPubSub:   &sakerhet.GCPPubSubIntegrationTestParams{},
 	})
 
-	pubSubC, err := i.GCPPubSubIntegrationTester.ContainerStart()
+	pubSubC, err := suite.IntegrationTester.GCPPubSubIntegrationTester.ContainerStart()
 	if err != nil {
-		t.Fatal(err)
+		suite.T().Fatal(err)
 	}
 
-	defer func() {
-		_ = pubSubC.Terminate(ctx)
-	}()
+	suite.GCPPubSubContainer = pubSubC
+}
 
+func (suite *GCPPubSubTestSuite) TearDownSuite() {
+	suite.GCPPubSubContainer.Terminate(suite.TestContext)
+}
+
+func TestGCPPubSubTestSuite(t *testing.T) {
+	suite.Run(t, new(GCPPubSubTestSuite))
+}
+
+// High level test on code that pushes to Pub/Sub
+func (suite *GCPPubSubTestSuite) TestHighLevelIntegrationTestGCPPubSub() {
 	wantedData := []byte(`{"myKey": "myValue"}`)
 
 	// when
-	if err := i.GCPPubSubIntegrationTester.PublishData(wantedData); err != nil {
-		t.Fatal(err)
+	if err := suite.IntegrationTester.GCPPubSubIntegrationTester.PublishData(wantedData); err != nil {
+		suite.T().Fatal(err)
 	}
 
 	// then
 	expectedData := [][]byte{[]byte(wantedData)}
-	if err := i.GCPPubSubIntegrationTester.ContainsWantedMessages(
+	if err := suite.IntegrationTester.GCPPubSubIntegrationTester.ContainsWantedMessages(
 		1*time.Second,
 		expectedData,
 	); err != nil {
-		t.Fatal(err)
-	}
-}
-
-// simple service that does a computation and publishes to Pub/Sub
-type myPowerOfNService struct {
-	toPowerOfN    func(float64, float64) float64
-	publishResult func(context.Context, string, string, string, float64) error
-}
-
-func newMyPowerOfNService() myPowerOfNService {
-	myPublishFunc := func(ctx context.Context, pubSubURI, projectID, topicID string, x float64) error {
-		conn, err := grpc.Dial(pubSubURI, grpc.WithTransportCredentials(insecure.NewCredentials()))
-		if err != nil {
-			return fmt.Errorf("grpc.Dial: %v", err)
-		}
-
-		o := []option.ClientOption{
-			option.WithGRPCConn(conn),
-			option.WithTelemetryDisabled(),
-		}
-
-		client, err := pubsub.NewClientWithConfig(ctx, projectID, nil, o...)
-		if err != nil {
-			return err
-		}
-		defer client.Close()
-
-		topic, err := abstractedcontainers.GetOrCreateGCPTopic(ctx, client, topicID)
-		if err != nil {
-			return err
-		}
-
-		payloadToPublish := []byte(fmt.Sprintf(`{"computationResult": %.2f}`, x))
-
-		if err := abstractedcontainers.PublishToGCPTopic(ctx, client, topic, payloadToPublish); err != nil {
-			return err
-		}
-
-		return nil
-	}
-
-	return myPowerOfNService{
-		toPowerOfN:    func(x float64, n float64) float64 { return math.Pow(x, n) },
-		publishResult: myPublishFunc,
+		suite.T().Fatal(err)
 	}
 }
 
 // High level test of a service that publishes to Pub/Sub
-func TestHighLevelIntegrationTestOfServiceThatUsesGCPPubSub(t *testing.T) {
-	// t.Parallel()
-
-	// given
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(time.Second*30))
-	defer cancel()
-
-	i := sakerhet.NewIntegrationTest(sakerhet.IntegrationTestParams{
-		TestContext: ctx,
-		GCPPubSub:   &sakerhet.GCPPubSubIntegrationTestParams{},
-	})
-
-	pubSubC, err := i.GCPPubSubIntegrationTester.ContainerStart()
-	if err != nil {
-		t.Fatal(err)
+func (suite *GCPPubSubTestSuite) TestHighLevelIntegrationTestOfServiceThatUsesGCPPubSub() {
+	// simple service that does a computation and publishes to Pub/Sub
+	type myPowerOfNService struct {
+		toPowerOfN func(float64, float64) float64
 	}
 
-	defer func() {
-		_ = pubSubC.Terminate(ctx)
-	}()
+	newMyPowerOfNService := func() myPowerOfNService {
+		return myPowerOfNService{
+			toPowerOfN: func(x float64, n float64) float64 { return math.Pow(x, n) },
+		}
+	}
 
 	// when
 	ponService := newMyPowerOfNService()
+	x := ponService.toPowerOfN(3, 3)
 
-	if err := ponService.publishResult(
-		ctx,
-		i.GCPPubSubIntegrationTester.PubSubURI,
-		i.GCPPubSubIntegrationTester.ProjectID,
-		i.GCPPubSubIntegrationTester.TopicID,
-		ponService.toPowerOfN(3, 3),
+	if err := suite.IntegrationTester.GCPPubSubIntegrationTester.PublishData(
+		[]byte(fmt.Sprintf(`{"computationResult": %.2f}`, x)),
 	); err != nil {
-		t.Fatal(err)
+		suite.T().Fatal(err)
 	}
 
 	// then
 	expectedData := [][]byte{[]byte(`{"computationResult": 27.00}`)}
-	if err := i.GCPPubSubIntegrationTester.ContainsWantedMessages(
+	if err := suite.IntegrationTester.GCPPubSubIntegrationTester.ContainsWantedMessages(
 		1*time.Second,
 		expectedData,
 	); err != nil {
-		t.Fatal(err)
+		suite.T().Fatal(err)
 	}
 }
 
@@ -201,8 +131,6 @@ func TestLowLevelIntegrationTestGCPPubSub(t *testing.T) {
 
 	fmt.Printf("New container started, accessible at: %s\n", pubSubC.URI)
 
-	// required so that all Pub/Sub calls go to docker container, and not GCP
-	// os.Setenv("PUBSUB_EMULATOR_HOST", pubSubC.URI)
 	conn, err := grpc.Dial(pubSubC.URI, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		t.Fatal(fmt.Errorf("grpc.Dial: %v", err))
@@ -232,7 +160,7 @@ func TestLowLevelIntegrationTestGCPPubSub(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	// theNn
+	// then
 	expectedData := [][]byte{[]byte(wantedData)}
 	if err := abstractedcontainers.CheckGCPMessageInSub(ctx, client, subscriptionID, expectedData, 1*time.Second); err != nil {
 		t.Fatal(err)
